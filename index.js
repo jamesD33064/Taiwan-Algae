6e6905c34cd0e8cb6a020a0f4adf929147d30901
Wei-Chin Huang <72090108+jamesD33064@users.noreply.github.com>
2021-11-26 00:25:25 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 function clo(id) {
     id.style.display = "none";
 }
+function more(id){
+    id.style.display = "block";
+}
+function onTopClick(flag) {
+    if(flag==1)
+    window.location.hash = "#i1";
+    else if(flag==2)
+    window.location.hash = "#i2";
+    else if(flag==3)
+    window.location.hash = "#i3";
+    else if(flag==4)
+    window.location.hash = "#i4";
+  }
