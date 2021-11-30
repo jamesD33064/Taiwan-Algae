5f1acd17c4babc9e6d027877efd8d5648e82ed88
Wei-Chin Huang <72090108+jamesD33064@users.noreply.github.com>
2021-11-30 11:27:37 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,31 @@
-function clo(id) {
-  id.style.display = "none";
-}
-function more(id) {
-  id.style.display = "block";
-}
 function onTopClick(flag) {
-  if (flag == 1) window.location.hash = "#i1";
-  else if (flag == 2) window.location.hash = "#i2";
-  else if (flag == 3) window.location.hash = "#i3";
+  if(flag==1){
+      const i3 = document.querySelector("#i1");
+      const height = i3.getBoundingClientRect().top + window.scrollY;
+      window.scrollBy({
+          left: 0,
+          top: height,
+          behavior: 'smooth',
+      });
+  }
+  else if(flag==2){
+      const i3 = document.querySelector("#i2");
+      const height = i3.getBoundingClientRect().top + window.scrollY;
+      window.scrollBy({
+          left: 0,
+          top: height,
+          behavior: 'smooth',
+      });
+  }
+  else if(flag==3){
+      const i3 = document.querySelector("#i3");
+      const height = i3.getBoundingClientRect().top + window.scrollY;
+      window.scrollBy({
+          left: 0,
+          top: height,
+          behavior: 'smooth',
+      });
+  }
   else if (flag == 4) {
     $("#vote_modal").modal("show");
     const dateText = document.querySelector("#date");
@@ -19,3 +37,16 @@ function onTopClick(flag) {
     dateText.textContent = `${days}天${hours}小時`;
   }
 }
+function clo(id) {
+  id.style.display = "none";
+}
+function more(id){
+  id.style.display = "block";
+}
+function toTop(){
+  var top = document.getElementById("view")
+  document.documentElement.scrollTop=top.offsetTop
+  window.pageYOffset=top.offsetTop
+  document.body.scrollTop=top.offsetTop ;
+}
+  
