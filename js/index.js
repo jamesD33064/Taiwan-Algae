a18957d8c30125ae5b04d37c75ff4e0de67d53fd
Wei-Chin Huang <72090108+jamesD33064@users.noreply.github.com>
2021-12-07 11:16:51 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,20 +1,51 @@
-function clo(id) {
-  id.style.display = "none";
-}
-function more(id) {
-  id.style.display = "block";
-}
 function onTopClick(flag) {
-  if (flag == 1) window.location.hash = "#i1";
-  else if (flag == 2) window.location.hash = "#i2";
-  else if (flag == 3) window.location.hash = "#i3";
-  else if (flag == 4) {
-    $("#vote_modal").modal("show");
-    const dateText = document.querySelector("#date");
-    const currentDate = new Date();
-    const voteDate = 1639756800000;
-    const days = parseInt((voteDate - currentDate) / 86400000);
-    const hours = parseInt((voteDate - currentDate) / 3600000 - days * 24);
-    dateText.textContent = `${days}天${hours}小時`;
-  }
+    if(flag==1){
+        const i3 = document.querySelector("#i1");
+        const height = i3.getBoundingClientRect().top + window.scrollY;
+        window.scrollBy({
+            left: 0,
+            top: height,
+            behavior: 'smooth',
+        });
+    }
+    else if(flag==2){
+        const i3 = document.querySelector("#i2");
+        const height = i3.getBoundingClientRect().top + window.scrollY;
+        window.scrollBy({
+            left: 0,
+            top: height,
+            behavior: 'smooth',
+        });
+    }
+    else if(flag==3){
+        const i3 = document.querySelector("#i3");
+        const height = i3.getBoundingClientRect().top + window.scrollY;
+        window.scrollBy({
+            left: 0,
+            top: height,
+            behavior: 'smooth',
+        });
+    }
+    else if(flag==4){
+        const i3 = document.querySelector("#i4");
+        const height = i3.getBoundingClientRect().top + window.scrollY;
+        window.scrollBy({
+            left: 0,
+            top: height,
+            behavior: 'smooth',
+        });
+    }
 }
+function clo(id) {
+    id.style.display = "none";
+}
+function more(id){
+    id.style.display = "block";
+}
+function toTop(){
+    var top = document.getElementById("view")
+    document.documentElement.scrollTop=top.offsetTop
+    window.pageYOffset=top.offsetTop
+    document.body.scrollTop=top.offsetTop ;
+}
+    
