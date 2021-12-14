4f72176c9a831eef8a2cfc2e9188ec5ffdb0dc01
Wei-Chin Huang <72090108+jamesD33064@users.noreply.github.com>
2021-12-14 11:32:49 +0800

    Add files via upload

---
+++ js/vote.js
@@ -0,0 +1,8 @@
+var sure=document.getElementById("sure");
+
+function sure_click(){
+    var obj=document.getElementById("frontselect_2");
+    var index = obj.selectedIndex;
+    var text = 
+    alert(index);
+}
